657d4d66e11b3cddddc943073d6557c87eb1a578
gianmarcoCossioArmijos <117977501+gianmarcoCossioArmijos@users.noreply.github.com>
2023-07-03 17:06:25 -0500

    reto 3 portafolio personal

---
+++ frontend-g14/semana5/dia2/app.js
@@ -0,0 +1,25 @@
+console.log("Hola Mundo");
+console.log(10%3);
+console.log(typeof("Hola"));
+console.log(typeof(15));
+console.log(typeof(3 > 2));
+console.log(3 == 2);
+console.log("hola" == "hola");
+
+//Conversion automatica o Coercion de tipo
+console.log("5" - 1);
+console.log("5" * 3);
+
+//let pemite crear variables que pueden cambiar de valor
+let age
+age = 100;
+
+let nombre
+nombre = "gianmarco";
+console.log(nombre + age);
+
+console.log(nombre = "cesar" + age);
+
+//const pemite crear variables pero su valor no es modificable
+const pais = "Peru";
+console.log(pais);
